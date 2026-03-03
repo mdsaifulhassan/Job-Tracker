6541b44039e8a023956d9ec72233f2d438f55f2a
mdsaifulhassan <saifulhasansaad@gmail.com>
2026-03-04 00:30:58 +0600

    Setup DOM element references in JavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+const totalValue = document.getElementById("totalValue");
+const interviewValue = document.getElementById("interviewValue");
+const rejectedValue = document.getElementById("rejectedValue");
+
+const listAll = document.getElementById("listAll");
+const listInterview = document.getElementById("listInterview");
+const listRejected = document.getElementById("listRejected");
+
+const jobParent = document.getElementById("jobParent");
+const template = document.getElementById("jobTemplate");
+const emptyState = document.getElementById("emptyState");
+const availableJobsCount = document.getElementById("availableJobsCount");
